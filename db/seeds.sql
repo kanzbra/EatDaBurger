4517511e186fbe073db50587e6cc63d3f285023f
kanzbra rashid <kanzbra.rashid@gmail.com>
2018-09-25 00:01:17 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burgerName) VALUES ('Chicken Burger');
+INSERT INTO burgers (burgerName) VALUES ('Turkey Burger');
+INSERT INTO burgers (burgerName, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burgerName, devoured) VALUES ('Fish Burger', true);
+
